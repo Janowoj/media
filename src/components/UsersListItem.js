28eda7cca90acd2439cb1b7d28016d37a026cbb3
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-12-07 09:18:31 +0100

    added a button to UsersListItem for deleting user with its functionality (deleteUser wrapped up with useThunk). Error with compoling to be fixed!

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,9 +1,25 @@
+import { GoTrashcan } from "react-icons/go";
+import Button from './Button';
+import { deleteUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
 function UsersListItem({ user }) {
-    return <div className='mb-2 border rounded'>
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
+    const [doDeleteUser, isLoading, error] = useThunk(deleteUser);
+
+    const handleClick = () => {
+        doDeleteUser(user);
+    }
+
+    return (
+        <div className='mb-2 border rounded'>
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+                <Button loading={isLoading} onClick={handleClick}>
+                    <GoTrashcan /> Delete
+                </Button>
+                {error && <div>Error deleting User...</div>}
+            </div>
         </div>
-    </div>
-};
+    );
+}
 
 export default UsersListItem;
