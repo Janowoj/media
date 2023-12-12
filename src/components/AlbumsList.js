b94ceadad80ee25cf926d8618550b9c01e607f6b
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-12-12 17:47:51 +0100

    RTK Query finished

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,11 @@
+import {useFetchAlbumsQuery} from '../store';
+
 function AlbumsList({ user }) {
-    return <div>
-        Albums created by {user.name}
-    </div>
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    
+    console.log(data, error, isLoading);
+    
+    return <div>Albums created by {user.name}</div>
 };
 
 export default AlbumsList;
