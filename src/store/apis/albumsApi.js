dd063bcb35949ad3b94cf9cd98e3e26c9e7a8a4a
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-12-28 23:19:40 +0100

    refetching completed

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -7,7 +7,7 @@ const albumsApi = createApi({
   endpoints(builder) {
     return {
       addAlbum: builder.mutation({
-        invalidatesTags: ['Album'],
+        invalidatesTags: (result, error, user) => [{ type: 'Album', id: user.id }],
         query: (user) => {
           return {
             url: '/albums',
@@ -20,7 +20,9 @@ const albumsApi = createApi({
         }
       }),
       fetchAlbums: builder.query({
-        providesTags: ['Album'],
+        providesTags: (result, error, user) => {
+          return [{ type: 'Album', id: user.id }];
+        },
         query: (user) => {
           return {
             url: '/albums',
